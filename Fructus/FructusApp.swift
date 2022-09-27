970aff1b4cf119f0e960986246b1c8d88d61c092
ozturkbasrii <ozturkbasrii@gmail.com>
2022-09-27 16:10:25 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Hasan Basri Öztürk on 27.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
